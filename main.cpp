188189f6d4e196d70d579887e8534b490c301c0f
candy800419 <candy800419@yahoo.com.tw>
2011-12-04 21:10:57 -0500

    more stuff

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <string.h>
 #include <time.h>
+#include <vector>
 #include "mem.h"
 using namespace std;
 
@@ -13,12 +14,16 @@ using namespace std;
 
 //Process* create(int size, int tp); //function to create an array of processes
 
-void firstFit(Process* p, int ptprob, int npprob);	//First Fit Algorithm
+//void firstFit(Process* p, int ptprob, int npprob);	//First Fit Algorithm
+void firstFit(vector<Process> &p, int ptprob, int npprob);
 void bestFit(Process* p, int ptprob, int npprob);	//Best Fit Algorithm
 void nextFit(Process* p, int ptprob, int npprob);	//Next Fit Algorithm
 void worstFit(Process* p, int ptprob, int npprob);	//Worst Fit Algorithm
 
 bool enterProbability(int prob);
+int nextProcessChar();
+int findFirstSlot(); //function that find the first location of an empty slot
+int findFirstSize(); //function that find the size of the first empty slot
 void printMem(); //function to print main memory
 
 int charArrLoc = 0; //counter for the ASCII character 
@@ -37,8 +42,8 @@ int main(int argc, char * argv[])
 		return 0;
 	}
 
-	ptp = (int)argv[2]; //process-termination-probability = argv[2]
-	npp = (int)argv[3]; //new-process-probability = argv[3]
+	ptp = atoi(argv[2]); //process-termination-probability = argv[2]
+	npp = atoi(argv[3]); //new-process-probability = argv[3]
 
 	//initialize main memory
 	for (i = 0; i < 2400; i++)
@@ -51,20 +56,21 @@ int main(int argc, char * argv[])
 
 	srand(time(NULL)); //set seed to get random numbers
 
-	Process* processes = new Process[58];	//create an array of Process
+	//Process* processes = new Process[58];	//create an array of Process
+	vector <Process> processes;
 	
 	//create and assign 20 processes into memory
 	for (int i = 0; i < 20; i++)
 	{
 		int w = nextProcessChar();
 		int amount = rand() % 90 + 10;
-		Process p(i+65, amount, used, ptp);
-		processes[i] = p;
+		Process p(i+65, amount, findFirstSlot(), ptp); 
+		//processes[i] = p;
+		processes.push_back(p);
 		for (j = 0; j < amount; j++)
 		{
-			mainMem[j+used] = w;
+			mainMem[findFirstSlot()] = w;
 		}
-		//used += amount;
 	}
 
 	//print out main memory
@@ -78,17 +84,17 @@ int main(int argc, char * argv[])
 	else if (strcmp(argv[1],"best") == 0)
 	{
 		//running best fit algo
-		bestFit(processes, ptp, npp);
+//		bestFit(processes, ptp, npp);
 	}
 	else if (strcmp(argv[1],"next") == 0)
 	{
 		//running next fit algo
-		nextFit(processes, ptp, npp);
+//		nextFit(processes, ptp, npp);
 	}
 	else if (strcmp(argv[1],"worst") == 0)
 	{
 		//running worst fit algo
-		worstFit(processes, ptp, npp);
+//		worstFit(processes, ptp, npp);
 	}
 	else
 	{
@@ -162,7 +168,31 @@ int nextProcessChar(){
 	return -1; //if -1 is returned the program is out of memory therefore it should terminate 
 }
 
-void firstFit(Process* p, int ptprob, int npprob)
+int findFirstSlot()
+{
+	int i = ros;
+
+	while(mainMem[i] != '.')
+	{
+		i++;
+	}
+	
+	return i;
+}
+
+int findFirstSize()
+{
+	int i = findFirstSlot(), size = 0;
+
+	while (mainMem[i] == '.')
+	{
+		size++;
+	}
+
+	return size;
+}
+
+/*void firstFit(Process* p, int ptprob, int npprob)
 {
 	char input;
 	cin >> input; 
@@ -170,23 +200,66 @@ void firstFit(Process* p, int ptprob, int npprob)
 	{
 		if (input == 'c')
 		{
+			//Check for terminated processes
+
+			//attempt to create new process
 			if(enterProbability(npprob) == true)
 			{ 
 				int k = nextProcessChar();
-				cout << "k = " << k << endl;
-				int random = rand() % 99 + 1;
-				Process newProcess(k+65, random, used, ptprob); 
-				for (int i = used; i < random; i++)
+				int random = rand() % 90 + 10;
+				Process newProcess(k+65, random, findFirstSlot(), ptprob); 
+				for (int i = 0; i < random; i++)
 				{
-					cout << "hey!!1" << endl;
-					mainMem[i] = k;
+					mainMem[findFirstSlot()] = k;
 				}
-				//create a new process
-				//then restart loop
 				printMem();
 			}
 			cin >> input;
-		}j
+		}
+		else
+		{
+			cout << "Can not understand your request!" << endl;
+			cin >> input;
+		}
+	}
+}*/
+
+void firstFit(vector<Process> &p, int ptprob, int npprob)
+{
+	char input;
+	cin >> input; 
+	while( input != 'q' )
+	{
+		if (input == 'c')
+		{
+			//Check for terminated processes
+			for (int j = 0; j < p.size(); j++)
+			{
+				if (enterProbability(p[j].getTermProb()) == true)
+				{
+					for (int m = 0; m < p[j].getCellRequired(); m++)
+					{
+						mainMem[p[j].getStartPos() + m] = '.';
+					}
+					p.erase(p.begin() + j);
+				}
+			}
+
+			//attempt to create new process
+			/*if(enterProbability(npprob) == true)
+			{ 
+				int k = nextProcessChar();
+				int random = rand() % 90 + 10;
+				Process newProcess(k+65, random, findFirstSlot(), ptprob);
+				p.push_back(newProcess);
+				for (int i = 0; i < random; i++)
+				{
+					mainMem[findFirstSlot()] = k;
+				}
+			}*/
+			printMem();
+			cin >> input;
+		}
 		else
 		{
 			cout << "Can not understand your request!" << endl;
