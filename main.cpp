eaeb5d49514872146269d893b790f85ece21dacd
candy800419 <candy800419@yahoo.com.tw>
2011-12-08 20:16:09 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 // Operating System FALL2011
 // Project #2 - Memory Management Simulation System
 // Tiffany Milano & Meng Jung Lin
-// Finalize Date: 12/09/2011
+// Finalize Date: 12/08/2011
 
 #include <iostream>
 #include <iomanip>
